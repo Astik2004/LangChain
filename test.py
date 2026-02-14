3ed3bd64f787594a2843a9d4bb59dc3daa60cc63
Astik Yadav <astikyadav4@gmail.com>
2026-02-14 22:35:14 +0530

    code changes for gemini

---
+++ test.py
@@ -0,0 +1,13 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    temperature=0.7
+)
+
+response = llm.invoke("Explain Java in one line")
+
+print(response.content)
